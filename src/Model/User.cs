e116c80641e8c1b8d88012a9fea322dbd1a95c21
Andrew <andrewklad@yandex.ru>
2025-03-30 23:27:22 +0300

    Изменено: в модели для пользователей и категорий трат добавлены навигационные свойства #6

--- src/Model/User.cs
+++ src/Model/User.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Model
@@ -14,5 +15,7 @@ namespace Model
         public int Id { get; set; }
         [AllowNull]
         public string? Token { get; set; }
+        [JsonIgnore]
+        public virtual ICollection<ExpenceCategory> ExpenceCategories { get; set; }
     }
 }
