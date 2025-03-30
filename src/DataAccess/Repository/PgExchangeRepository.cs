7d8faccbad331ed8b50717ec9b02bc191a2380b3
Andrew <andrewklad@yandex.ru>
2025-03-31 01:52:35 +0300

    Добавлено: реализация репозитория Exchange (без интеграций, просто заглушки)

---
+++ src/DataAccess/Repository/PgExchangeRepository.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class PgExchangeRepository : IExchangeRepository
+    {
+        private PgPayContext _ctx;
+
+        public PgExchangeRepository(PgPayContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public bool ExchangeAuth(string token)
+        {
+            return false;
+        }
+
+        public JsonObject? GetBalance(int userId)
+        {
+            return null;
+        }
+    }
+}
