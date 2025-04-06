5e033a8f8b754a884d718232b4b02c849c8beeaa
Andrew <andrewklad@yandex.ru>
2025-04-06 21:56:14 +0300

    Добавлено: Контроллер для Binance

---
+++ src/Controllers/ExchangeController.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess;
+using DataAccess.Interface;
+using DataAccess.Repository;
+using Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model.DTO;
+using System.Text.Json.Nodes;
+
+namespace src.Controllers
+{
+    [ApiController]
+    public class ExchangeController : ControllerBase
+    {
+        private IExchangeRepository _rep;
+
+        public ExchangeController(PgPayContext ctx) : base()
+        {
+            _rep = new PgExchangeRepository(ctx);
+        }
+
+        [Route("/api/exchange/balance")]
+        public ActionResult<JsonObject> GetBalance([FromQuery] int userId)
+        {
+            var res = _rep.GetBalance(userId);
+            return res != null ? Ok(res) : BadRequest(res);
+        }
+
+        [Route("/api/exchange/auth")]
+        public ActionResult MakeAuth([FromQuery] int userId, [FromBody] string token)
+        {
+            return _rep.ExchangeAuth(userId, token) ? Ok() : BadRequest();
+        }
+    }
+}
