4f24c2dcd7f8527e38e8f47811a6a48ca1b1a150
elizavetaladygina <lizalad07@gmail.com>
2025-04-07 10:37:04 +0300

    Добавлено: классы генерации объектов по паттерну DataBuilder

---
+++ tests/TestDataGenerators/DataBuilders/DTO_Builders/CurrencyPairDtoBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Model.DTO;
+
+namespace TestDataGenerators.DataBuilders.DTO_Builders
+{
+	public class CurrencyPairDtoBuilder
+	{
+		private CurrencyPairDto _currencyPairDto = new CurrencyPairDto();
+
+		public CurrencyPairDtoBuilder WithCur1(string cur1)
+		{
+			_currencyPairDto.Cur1 = cur1;
+			return this;
+		}
+
+        public CurrencyPairDtoBuilder WithCur2(string cur2)
+        {
+            _currencyPairDto.Cur2 = cur2;
+            return this;
+        }
+
+        public CurrencyPairDto Build()
+		{
+			return _currencyPairDto;
+		}
+    }
+}
+
