3f728ccc2ed4f4a1bedd8a2890fcf51a72ed9ccd
elizavetaladygina <lizalad07@gmail.com>
2025-04-14 09:50:32 +0300

    Добавлено: классы генерации объектов по паттерну ObjectMother

---
+++ tests/TestDataGenerators/ObjectMothers/DTO_ObjectMothers/CurrencyPairDtoObjectMother.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Model.DTO;
+
+namespace TestDataGenerators.ObjectMothers.DTO_ObjectMothers
+{
+	public class CurrencyPairDtoObjectMother
+	{
+        public static CurrencyPairDto CreateCurrencyPair()
+        {
+            return new CurrencyPairDto
+            {
+                Cur1 = "USD",
+                Cur2 = "EUR"
+            };
+        }
+
+        public static CurrencyPairDto CreateCurrencyPairWithCurrencies(string currency1, string currency2)
+        {
+            return new CurrencyPairDto
+            {
+                Cur1 = currency1,
+                Cur2 = currency2
+            };
+        }
+    }
+}
+
---
+++ tests/TestDataGenerators/ObjectMothers/UserObjectMother.cs
@@ -0,0 +1,111 @@
+﻿using Model;
+
+namespace TestDataGenerators.ObjectMothers
+{
+	public static class UserObjectMother
+	{
+        private static int _nextId = 1;
+
+        public static User CreateUser()
+		{
+            return new User
+            {
+                Id = _nextId++,
+                Token = "testToken-12345",
+                ExpenceCategories = new List<ExpenceCategory>(),
+                Expences = new List<Expence>(),
+                Subscriptions = new List<Subscription>(),
+                ExchangeRates = new List<ExchangeRateSubscription>()
+            };
+        }
+
+        public static User CreateUserWithCollections(
+            ICollection<ExpenceCategory> categories = null,
+            ICollection<Expence> expenses = null,
+            ICollection<Subscription> subscriptions = null,
+            ICollection<ExchangeRateSubscription> exchangeRates = null)
+        {
+            return new User
+            {
+                Id = _nextId++,
+                Token = "testToken-23456",
+                ExpenceCategories = categories ?? new List<ExpenceCategory>(),
+                Expences = expenses ?? new List<Expence>(),
+                Subscriptions = subscriptions ?? new List<Subscription>(),
+                ExchangeRates = exchangeRates ?? new List<ExchangeRateSubscription>()
+            };
+        }
+
+        public static User CreateUserWithToken(string token)
+        {
+            return new User
+            {
+                Id = _nextId++,
+                Token = token,
+                ExpenceCategories = new List<ExpenceCategory>(),
+                Expences = new List<Expence>(),
+                Subscriptions = new List<Subscription>(),
+                ExchangeRates = new List<ExchangeRateSubscription>()
+            };
+        }
+
+        public static User CreateUserWithExpenseCategories(ICollection<ExpenceCategory> categories)
+        {
+            return new User
+            {
+                Id = _nextId++,
+                Token = "testToken-45678",
+                ExpenceCategories = categories ?? new List<ExpenceCategory>(),
+                Expences = new List<Expence>(),
+                Subscriptions = new List<Subscription>(),
+                ExchangeRates = new List<ExchangeRateSubscription>()
+            };
+        }
+
+        public static User CreateUserWithExpenses(ICollection<Expence> expenses)
+        {
+            return new User
+            {
+                Id = _nextId++,
+                Token = "testToken-56789",
+                ExpenceCategories = new List<ExpenceCategory>(),
+                Expences = expenses ?? new List<Expence>(),
+                Subscriptions = new List<Subscription>(),
+                ExchangeRates = new List<ExchangeRateSubscription>()
+            };
+        }
+
+        public static User CreateUserWithSubscriptions(ICollection<Subscription> subscriptions)
+        {
+            return new User
+            {
+                Id = _nextId++,
+                Token = "testToken-67890",
+                ExpenceCategories = new List<ExpenceCategory>(),
+                Expences = new List<Expence>(),
+                Subscriptions = subscriptions ?? new List<Subscription>(),
+                ExchangeRates = new List<ExchangeRateSubscription>()
+            };
+        }
+
+        public static User CreateUserWithExchangeRates(ICollection<ExchangeRateSubscription> exchangeRates)
+        {
+            return new User
+            {
+                Id = _nextId++,
+                Token = "testToken-78901",
+                ExpenceCategories = new List<ExpenceCategory>(),
+                Expences = new List<Expence>(),
+                Subscriptions = new List<Subscription>(),
+                ExchangeRates = exchangeRates ?? new List<ExchangeRateSubscription>()
+            };
+        }
+
+
+        public static void ResetIdSequence()
+        {
+            _nextId = 1;
+        }
+    }
+}
+
