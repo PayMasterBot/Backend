8ffededcfea86b245142c6864d6e3b62b13c452b
Andrew <andrewklad@yandex.ru>
2025-04-14 12:22:33 +0300

    Добавлено: интеграции с Binance для получения обменных курсов и отчета о нем

--- src/DataAccess/Repository/PgExchangeRepository.cs
+++ src/DataAccess/Repository/PgExchangeRepository.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 
@@ -19,11 +20,23 @@ namespace DataAccess.Repository
 
         public bool ExchangeAuth(int userId, string token)
         {
+            //
             return false;
         }
 
         public JsonObject? GetBalance(int userId)
         {
+            //https://api.binance.com/api/v3/account
+            // => "balances"
+            using (var cli = new HttpClient())
+            {
+                var data = cli.GetAsync($"https://api.binance.com/api/v3/account");
+                data.Wait();
+                using (var doc = JsonDocument.Parse(data.Result.Content.ReadAsStream()))
+                {
+                    doc.RootElement.GetProperty("price").GetDouble();
+                }
+            };
             return null;
         }
     }
