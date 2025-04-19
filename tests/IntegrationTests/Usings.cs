1696d78ea0da050bf5c35cafa840c67c5e8ae961
elizavetaladygina <lizalad07@gmail.com>
2025-04-20 00:20:15 +0300

    Добавлено: проект для интеграционных тестов

---
+++ tests/IntegrationTests/Usings.cs
@@ -0,0 +1,19 @@
+﻿global using NUnit.Framework;
+
+global using Microsoft.AspNetCore.Mvc.Testing;
+global using Microsoft.AspNetCore.Hosting;
+global using Newtonsoft.Json;
+global using System.Net;
+global using System.Net.Http.Json;
+global using Microsoft.Extensions.DependencyInjection;
+global using src;
+global using Model;
+global using DataAccess;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.HttpsPolicy;
+
+global using Microsoft.EntityFrameworkCore.InMemory;
+global using Allure.NUnit;
+
+global using TestDataGenerators.DataBuilders;
+global using TestDataGenerators.ObjectMothers;
